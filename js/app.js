b55231d6d1c13c0255b91a831eec389338673d07
Naresh Singh <nareshsingh0170@gmail.com>
2024-02-05 12:01:00 +0530

    Nav Done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-//Js File
+// Nav Bar
+
+let navBar = document.querySelector("nav");
+let humburger = document.querySelector(".sub-hamburger");
+
+humburger.addEventListener('click', () => {
+    navBar.classList.toggle("active");
+})
+
