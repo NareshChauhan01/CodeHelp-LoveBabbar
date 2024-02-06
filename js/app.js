1c86351934d32c60f35c8253e4e9a0d17bd64073
Naresh Singh <nareshsingh0170@gmail.com>
2024-02-06 12:00:51 +0530

    hero section complete

--- js/app.js
+++ js/app.js
@@ -7,3 +7,15 @@ humburger.addEventListener('click', () => {
     navBar.classList.toggle("active");
 })
 
+// Slide-Text
+var typeData = new Typed(".role", {
+    strings: [
+        "Love",
+        "Babber"
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+});
+
